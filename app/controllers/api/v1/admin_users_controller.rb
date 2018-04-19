00772a2be0f9e00be1f458d02d3d59ee2fc8582f
nhoxtien2010 <nhoxtien2010@gmail.com>
2018-04-19 13:59:24 +0700

    create login API

--- app/controllers/api/v1/admin_users_controller.rb
+++ app/controllers/api/v1/admin_users_controller.rb
@@ -1,9 +1,21 @@
 class Api::V1::AdminUsersController < Api::Base
-  before_action :authenticate, except: :create
-  
+  before_action :authenticate, except: %i[create login]
+
   def create
     user = AdminUser.create!(user_params)
     render json: user
+
+  end
+
+
+  def login
+    user = AdminUser.find_by_email(user_params[:email])
+
+    if user && user.validate(user_params[:password])
+      render json: {success: true, token: user.authentication_token}
+    else
+      render json: {success: false, error: 'Wrong user email or password'}
+    end
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :admin_users, only: %i[create]
+      post 'admin_users/login'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
